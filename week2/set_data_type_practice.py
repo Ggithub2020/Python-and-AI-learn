a86bf8820fd59580ef7a0dc9b581b37ea6da3d6c
Girum <giruml2020@gmail.com>
2025-12-25 22:12:08 -0800

    added week2 practice files

---
+++ week2/set_data_type_practice.py
@@ -0,0 +1,35 @@
+
+# set  example
+#set {  }
+# set1= {1,2,3,4,5,6}
+# set2= {4,5,6,7,8,9}
+# set1.add(7)
+# for s in set1:
+#     print(s)
+# set1.remove(3)
+# for s in set1:
+#     print(s)
+# set3= set1.union(set2)
+# print("Union of set1 and set2 is:", set3)
+# set4= set1.intersection(set2)
+# print("Intersection of set1 and set2 is:", set4)    
+# coffee_types = {"Espresso", "Latte", "Cappuccino", "Americano"}
+# print("Available coffee types:")
+# for coffee in coffee_types:
+#     print(coffee)
+# order = input("Please enter the coffee type you want: ")
+# if order in coffee_types:
+#     print(f"Preparing your {order}!")
+# number1={1,2,3,4,5,6,7,8,9,10}
+# number2={4,5,6,7,8}
+# # ns=number1.union(number2)
+# # nn=number1.intersection(number2)
+# nw=number1.difference(number2)
+# print("Difference of number1 and number2 is:")
+# # print("Union of number1 and number2 is:")
+# # for n in ns:
+# #     print(n)
+# # for n in nn:
+# #     print(n)
+# for n in nw:
+#     print(n)
